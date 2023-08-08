ab90a70f91702e42800bc12f14ffd2c0124d1236
ZombieNW <59242209+ZombieNW@users.noreply.github.com>
2023-08-07 21:01:30 -0700

    Fix Taskbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script>
     import Taskbar from '../components/Taskbar.svelte';
-    import TaskbarIcon from '../components/Taskbar.svelte';
+    import TaskbarIcon from '../components/TaskbarIcon.svelte';
+    import DesktopIcon from '../components/Taskbar.svelte';
 </script>
 
 <style lang="postcss">
@@ -8,8 +9,14 @@
         background-image: url("../assets/background.jpg");
         background-repeat: no-repeat;
         background-size: cover;
+        margin: 0;
     }
 </style>
+
+<!--Desktop icons-->
+<div class="h-screen w-full m-0">
+    <DesktopIcon icon="/assets/logo_1080x.png" title="Application Title"/>
+</div>
 
 <Taskbar>
     <TaskbarIcon icon="/assets/logo_1080x.png" title="Application Title"/>
